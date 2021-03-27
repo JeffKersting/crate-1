520e8f106c0f17468d7847b78bb68c695c933b9e
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-27 17:36:52 -0600

    Add survey render logic

--- code/web/src/modules/survey/List.js
+++ code/web/src/modules/survey/List.js
@@ -3,6 +3,8 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
+import axios from 'axios'
+import { query, mutation } from 'gql-query-builder'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
@@ -15,6 +17,7 @@ import Loading from '../common/Loading'
 import EmptyMessage from '../common/EmptyMessage'
 import SurveyItem from './Item'
 import { create } from '../survey/api/actions'
+import { routeApi } from '../../../setup/routes'
 
 // Component
 class List extends PureComponent {
@@ -23,18 +26,53 @@ class List extends PureComponent {
     super(props)
 
     this.state = {
-      isLoading: false,
-      genderStyle: '',
+      genderStyle: 0,
       surveyData: [],
       casual: 0,
       edgy: 0,
-      street: 0,
-      display: 0
+      streetwear: 0,
+      display: ''
     }
   }
 
-  onClick(event) => {
-    this.setState({event.target.id++}, {display++})  
+  setDisplay = () => {
+    const total = this.state.casual + this.state.edgy + this.state.streetwear
+    switch(total) {
+      case 0:
+        this.setState({display: 'gender'})
+        break
+      case 1:
+        this.setState({display: 'shirts'})
+        break
+      case 2:
+        this.setState({display: 'bottoms'})
+        break
+      case 0:
+        this.setState({display: 'accessories'})
+        break
+      case 0:
+        this.setState({display: 'shoes'})
+        break
+      default:
+        break
+    }
+  }
+
+  onClick = (event) => {
+    const target = event.target.id
+    this.setState({target: state.target++)
+    setDisplay()
+  }
+
+  filterSurveyDisplay = () => {
+    const nonBinary = []
+    if(this.state.genderStyle === 0) {
+
+    } else {
+      return this.state.surveyData.filter(data =>
+        data.gender === this.state.genderStyle || data.type === this.state.display
+      )
+    }
   }
 
   onSubmit() {
@@ -51,7 +89,13 @@ class List extends PureComponent {
 
   // Runs on client only
   componentDidMount() {
-    this.props.getSurveyList('ASC')
+    return axios.post(routeApi, query({
+      operation: '',
+      fields: ['id', 'name', 'image', 'gender']
+    }))
+      .then(response => {
+        setState({surveyData: response.styles})
+      })
   }
 
   render() {
@@ -59,16 +103,14 @@ class List extends PureComponent {
       <div>
         {/* SEO */}
         <Helmet>
-          <title>Crates for everyone! - Crate</title>
+          <title>Style Survey</title>
         </Helmet>
 
         {/* Top title bar */}
         <Grid style={{ backgroundColor: grey }}>
           <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-            <H3 font="secondary">Crates for everyone!</H3>
-
-            <p style={{ marginTop: '1em', color: grey2 }}>You can choose crate which suits your need. You can also
-              subscribe to multiple crates.</p>
+            <H3 font="secondary">Style Survey</H3>
+            <p style={{ marginTop: '1em', color: grey2 }}>Select {this.state.display} style preference</p>
           </GridCell>
         </Grid>
 
@@ -76,15 +118,15 @@ class List extends PureComponent {
         <Grid>
           <GridCell>
             {
-              this.props.crates.isLoading
+              this.state.surveyData.length
                 ? <Loading/>
-                : this.props.crates.list.length > 0
-                    ? this.props.crates.list.map(crate => (
-                      <div key={crate.id} style={{ margin: '2em', float: 'left' }}>
-                        <CrateItem crate={crate}/>
+                : this.state.surveyData.length > 0
+                    ? this.state.surveyData.map(info => (
+                      <div key={info.id} style={{ margin: '2em', float: 'left' }}>
+                        <SurveyItem info={info}/>
                       </div>
                     ))
-                    : <EmptyMessage message="No crates to show" />
+                    : <EmptyMessage message="No survey to show" />
             }
           </GridCell>
         </Grid>
