72a0a9d001e8b61decf27e82371a8a23240a8f0c
Ely Hess <ely.hess@me.com>
2021-03-29 17:09:47 -0600

    Add userUpdate mutation
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,7 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-import { create, remove } from './resolvers'
+import { create, remove, update } from './resolvers'
 
 // Create
 export const userSignup = {
@@ -26,6 +26,23 @@ export const userSignup = {
   },
   resolve: create
 }
+
+// Update
+export const userUpdate = {
+  type: UserType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    },
+    style: {
+      style: 'style',
+      type: GraphQLString
+    }
+  },
+  resolve: update
+}
+
 
 // Remove
 export const userRemove = {
